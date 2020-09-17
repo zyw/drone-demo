6bda2a13d885ab21f85c354de3e9a0cc90c202f5
AS <zyw090111@163.com>
2020-09-17 15:51:35 +0800

    编写.drone.yml文件

--- src/main/java/cn/v5cn/drone/demo/controller/IndexController.java
+++ src/main/java/cn/v5cn/drone/demo/controller/IndexController.java
@@ -3,11 +3,27 @@ package cn.v5cn.drone.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 @RestController
 public class IndexController {
 
     @GetMapping("/index")
-    public Object index(){
+    public Object index() throws IOException {
+
+        File flie = new File("/mnt/data");
+        if(!(!flie.exists() && flie.mkdirs())) {
+            throw new RuntimeException("创建目录失败");
+        }
+        File text = new File(flie + "/hello.txt");
+        FileOutputStream fos = new FileOutputStream(text);
+        fos.write("Hello World".getBytes(StandardCharsets.UTF_8));
+        fos.flush();
+        fos.close();
         return "Hello Drone";
     }
 }
